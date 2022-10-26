52de9ecc90ae315057087305f817ef20755f0870
Marygimm <marygimm.moreira@bringglobal.com>
2022-10-26 14:10:04 +0000

    create slot machine mini game

---
+++ SlotMachine/Helpers/Extensions.swift
@@ -0,0 +1,23 @@
+//
+//  Extensions.swift
+//  SlotMachine
+//
+//  Created by Mary Moreira on 26/10/2022.
+//
+
+import SwiftUI
+
+extension Text {
+  func scoreLabelStyle() -> Text {
+    self
+      .foregroundColor(Color.white)
+      .font(.system(size: 10, weight: .bold, design: .rounded))
+  }
+  
+  func scoreNumberStyle() -> Text {
+    self
+      .foregroundColor(Color.white)
+      .font(.system(.title, design: .rounded))
+      .fontWeight(.heavy)
+  }
+}
---
+++ SlotMachine/SlotMachineApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlotMachineApp.swift
+//  SlotMachine
+//
+//  Created by Mary Moreira on 26/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct SlotMachineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
